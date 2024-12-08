1456ee6b9a2f10b986ebfbf356500cc6a5e15828
regexhater <saribander@gmail.com>
2024-12-09 00:29:23 +0100

    Day 3 Part 2

--- day3/src/Main.kt
+++ day3/src/Main.kt
@@ -3,6 +3,10 @@ fun main() {
     val part1Result = part1(lines)
     println("Day 3 Part 1")
     println("Result = $part1Result")
+
+    val part2Result = part2(lines)
+    println("Day 3 Part 2")
+    println("Result = $part2Result")
 }
 
 fun part1(lines: List<String>): Int {
@@ -11,10 +15,37 @@ fun part1(lines: List<String>): Int {
     for (line in lines) {
         val matchResult = pattern.findAll(line)
         for (match in matchResult) {
-            val mulStr = match.value.replace("mul(", "").replace(")", "")
-            val split = mulStr.split(",")
-            sum += split[0].toInt() * split[1].toInt()
+            sum += convertAndMultiply(match.value)
         }
     }
     return sum
+}
+
+fun part2(lines: List<String>): Int {
+    val mulPattern = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+    val enablePattern = Regex("do\\(\\)|don't\\(\\)")
+    var enable = true
+    var sum = 0
+    for (line in lines) {
+        val mulMatchResult = mulPattern.findAll(line)
+        val enableMatchList =  enablePattern.findAll(line).toMutableList()
+        for (match in mulMatchResult) {
+            val foundLogicOperator = enableMatchList.firstOrNull { it.range.last < match.range.first }
+            if (foundLogicOperator != null) {
+                enableMatchList.remove(foundLogicOperator)
+                enable = foundLogicOperator.value == "do()"
+            }
+            if (!enable) {
+                continue
+            }
+            sum += convertAndMultiply(match.value)
+        }
+    }
+    return sum
+}
+
+fun convertAndMultiply(x: String): Int {
+    val mulStr = x.replace("mul(", "").replace(")", "")
+    val split = mulStr.split(",")
+    return split[0].toInt() * split[1].toInt()
 }
